545a332b54ceeabe6948d183731824eea5d59490
heyu <heyu@qq.com>
2020-06-11 22:26:45 +0800

    2020.6.11 22:26已提交

--- utils/request.js
+++ utils/request.js
@@ -1,9 +1,13 @@
 const config=require("./config.js");
+let token=wx.getStorageSync('token');
 module.exports=(url,data,method="GET") => {
   return new Promise((resolve,reject) => {
     wx.request({
       url:config.host+url,
       data,
+      header:{
+        authorization:token
+      },
       success(res){
         resolve(res.data);
       },
